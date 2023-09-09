7ff5c1f9a534080d5bf6d689610e054151c2b42f
9000HAL <gsanchezscs@gmail.com>
2023-09-08 19:59:23 -0700

    error here re: render_template...?

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
+import psycopg2
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+
+
+# Database configuration
+DATABASE_URL = "postgres://tagzmraj:U7pLZTuQTLxrKR5Dzd0E70awv1WzJPkv@bubble.db.elephantsql.com/tagzmraj"
+
+
+
 @app.route('/')
 def home():
-    return "Hello, this is the starting point!"
+
+# Fetch data from an example API: JSONPlaceholder
+    api_url = "https://jsonplaceholder.typicode.com/todos/1"
+    response = requests.get(api_url)
+    api_data = response.json()
+
+    return render_template('index.html', api_data=api_data)
+
+
+    # Connect to PostgreSQL
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    version = cur.fetchone()
+    conn.close()
+    return f"Connected to PostgreSQL version: {version[0]}"
 
 if __name__ == "__main__":
     app.run(debug=True)
