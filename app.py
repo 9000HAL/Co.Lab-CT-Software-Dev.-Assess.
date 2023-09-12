d72e4ceb73d84128b8621a6cadf0c3c5c93c6e16
9000HAL <gsanchezscs@gmail.com>
2023-09-11 22:57:00 -0700

    closing save to host to github pages/green go 11pm

--- app.py
+++ app.py
@@ -1,34 +1,127 @@
+# Description: Flask App for API     v.2
+
 import psycopg2
 from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
-
-
 # Database configuration
 DATABASE_URL = "postgres://tagzmraj:U7pLZTuQTLxrKR5Dzd0E70awv1WzJPkv@bubble.db.elephantsql.com/tagzmraj"
-
-
 
 @app.route('/')
 def home():
 
-# Fetch data from an example API: JSONPlaceholder
-    api_url = "https://jsonplaceholder.typicode.com/todos/1"
-    response = requests.get(api_url)
-    api_data = response.json()
-
-    return render_template('index.html', api_data=api_data)
-
-
+    # Fetch multiple data items from an example API: JSONPlaceholder
+    response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    api_data = response.json()[0:3]  # Fetch first 3 todo items
+    
     # Connect to PostgreSQL
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
     cur.execute("SELECT version();")
     version = cur.fetchone()
     conn.close()
-    return f"Connected to PostgreSQL version: {version[0]}"
+
+    return render_template('index.html', api_data=api_data, version=version[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
